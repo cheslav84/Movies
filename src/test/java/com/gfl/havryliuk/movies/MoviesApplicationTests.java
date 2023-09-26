d77a61205f0f11c7233637347db65764da3b543f
Viacheslav Havryliuk <v.cheslav84@gmail.com>
2023-09-26 17:45:54 +0300

    init srping

---
+++ src/test/java/com/gfl/havryliuk/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfl.havryliuk.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
